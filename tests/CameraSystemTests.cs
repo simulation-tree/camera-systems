6435dbfe9b5fb212e9fc1261d89533f765c4a2ed
Phill <philldamaskin@gmail.com>
2025-02-03 20:36:32 -0500

    initial commit

---
+++ tests/CameraSystemTests.cs
@@ -0,0 +1,36 @@
+﻿using Cameras.Systems;
+using Rendering;
+using Simulation.Tests;
+using Transforms;
+using Transforms.Systems;
+using Types;
+using Worlds;
+
+namespace Cameras.Systems.Tests
+{
+    public abstract class CameraSystemTests : SimulationTests
+    {
+        static CameraSystemTests()
+        {
+            TypeRegistry.Load<TransformsTypeBank>();
+            TypeRegistry.Load<RenderingTypeBank>();
+            TypeRegistry.Load<CamerasTypeBank>();
+        }
+
+        protected override Schema CreateSchema()
+        {
+            Schema schema = base.CreateSchema();
+            schema.Load<TransformsSchemaBank>();
+            schema.Load<RenderingSchemaBank>();
+            schema.Load<CamerasSchemaBank>();
+            return schema;
+        }
+
+        protected override void SetUp()
+        {
+            base.SetUp();
+            simulator.AddSystem<TransformSystem>();
+            simulator.AddSystem<CameraSystem>();
+        }
+    }
+}
